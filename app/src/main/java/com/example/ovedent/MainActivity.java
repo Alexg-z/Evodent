8426e65d0a422b223529315f5d12bb14b5d36534
AlejandroG <alex.lags@outlook.es>
2022-10-12 23:20:58 -0600

    Changes

--- app/src/main/java/com/example/ovedent/MainActivity.java
+++ app/src/main/java/com/example/ovedent/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.ovedent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etxUsr, etxPass;
+    CheckBox chbSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etxUsr = (EditText) findViewById(R.id.etxUsr);
+        etxPass = (EditText) findViewById(R.id.etxPass);
+        chbSession = (CheckBox) findViewById(R.id.chbSession);
     }
+
+    // Function Login
+    public void login (View view) {
+
+
+    }
+
+    // Function Register
+    public void register (View view) {
+
+    }
+
 }
